a9f47dd8f185b10815acfd710192e4528f013a44
siobahnschuck <sschuc@saic.edu>
2021-03-04 17:56:58 -0600

    home page mvp

--- client_timeCapped/src/screens/Home.jsx
+++ client_timeCapped/src/screens/Home.jsx
@@ -20,9 +20,7 @@ export default class Home extends Component{
                   internet voyage. <br/> <br/> As an active time capsule any visitor to the capsule is welcomed to trade a piece of media for something out of the capsule. You might uncover 
                   something rare, a personal anecdote from another visitor, or a relic of meme history. <br/> <br/>There is only one way to find out!</p>
                   </section>
-                  {/* <section className="enterCap"> */}
                   <NavLink to="/capsule" className="cap-txt">Enter the capsule</NavLink>
-                  {/* </section> */}
                   <div className="graphic" >
                   <img className="bliss" src={graphic} alt="home-Page-Graphic" border="0"/>
                   </div>
