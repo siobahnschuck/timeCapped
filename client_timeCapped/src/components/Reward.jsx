ae484e54635a66f89ae629752c12f71f76554aa1
siobahnschuck <sschuc@saic.edu>
2021-03-05 11:47:46 -0600

    mvp

--- client_timeCapped/src/components/Reward.jsx
+++ client_timeCapped/src/components/Reward.jsx
@@ -19,12 +19,12 @@ export default class Reward extends Component {
                         console.log(value)
                         return (<div className="imgReward"> <div> <img src={value} alt="poster" style={{height: 350, width: 350}} /> </div>
                           <div className="social">
-                            <FacebookShareButton url="http://localhost:3000/capsule" quote={"Look what I got out of the capsule!"}
+                            <FacebookShareButton className="social-btn" url="http://localhost:3000/capsule" quote={"Look what I got out of the capsule!"}
                             hashtag="#React #NetArt">
-                            <FacebookIcon logoFillColor="white" round={true}></FacebookIcon>
+                            <FacebookIcon logoFillColor="white" round={true} style={{boxShadow: 'none'}}></FacebookIcon>
                             </FacebookShareButton>
-                            <TwitterShareButton title="Internet Capsule" via="http://localhost:3000/capsule" hashtags="[#React #NetArt]"><TwitterIcon logoFillColor="white" round={true}></TwitterIcon></TwitterShareButton>
-                            <EmailShareButton subject="Look at what I pulled from this Internet Time Capsule!"><EmailIcon logoFillColor="white" round={true}></EmailIcon></EmailShareButton> 
+                            <TwitterShareButton className="social-btn" title="Internet Capsule" via="http://localhost:3000/capsule" hashtags="[#React #NetArt]"><TwitterIcon logoFillColor="white" round={true}></TwitterIcon></TwitterShareButton>
+                            <EmailShareButton className="social-btn" subject="Look at what I pulled from this Internet Time Capsule!"><EmailIcon logoFillColor="white" round={true}></EmailIcon></EmailShareButton> 
                             <button className="delete" ><img className="trash" src="https://img.icons8.com/wired/64/000000/delete-forever.png" alt="trash" onClick={this.props.imgDelete}/></button>
                             <button type="button" className="submit-btn" onClick={this.props.startOverClick}>RESET</button>
                             </div> 
@@ -34,12 +34,12 @@ export default class Reward extends Component {
                         return (
                         <div className="linkReward"> <div><a href={value} target="_blank" rel="noreferrer" style={{fontSize: 50}}> {value} </a></div>
                           <div className="social">
-                          <FacebookShareButton url="http://localhost:3000/capsule" quote={"Look what I got out of the capsule!"}
+                          <FacebookShareButton  className="social-btn" url="http://localhost:3000/capsule" quote={"Look what I got out of the capsule!"}
                           hashtag="#React #NetArt">
-                          <FacebookIcon logoFillColor="white" round={true}></FacebookIcon>
+                          <FacebookIcon logoFillColor="white" round={true} style={{boxShadow: 'none'}}></FacebookIcon>
                           </FacebookShareButton>
-                          <TwitterShareButton title="Internet Capsule" via="http://localhost:3000/capsule" hashtags="[#React #NetArt]"><TwitterIcon logoFillColor="white" round={true}></TwitterIcon></TwitterShareButton>
-                          <EmailShareButton subject="Look at what I pulled from this Internet Time Capsule!"><EmailIcon logoFillColor="white" round={true}></EmailIcon></EmailShareButton>
+                          <TwitterShareButton  className="social-btn" title="Internet Capsule" via="http://localhost:3000/capsule" hashtags="[#React #NetArt]"><TwitterIcon logoFillColor="white" round={true}></TwitterIcon></TwitterShareButton>
+                          <EmailShareButton className="social-btn" subject="Look at what I pulled from this Internet Time Capsule!"><EmailIcon logoFillColor="white" round={true}></EmailIcon></EmailShareButton>
                           <button className="delete" ><img className="trash" src="https://img.icons8.com/wired/64/000000/delete-forever.png" alt="trash" onClick={this.props.linkDelete}/></button>
                           <button type="button" className="submit-btn" onClick={this.props.startOverClick}>RESET</button>
                           </div>
@@ -49,12 +49,12 @@ export default class Reward extends Component {
                         return (
                         <div className="text-reward"><p style={{fontSize: 20}}> {value} </p>
                           <div className="social">
-                          <FacebookShareButton url="http://localhost:3000/capsule" quote={"Look what I got out of the capsule!"}
+                          <FacebookShareButton className="social-btn" url="http://localhost:3000/capsule" quote={"Look what I got out of the capsule!"}
                           hashtag="#React #NetArt">
-                          <FacebookIcon logoFillColor="white" round={true}></FacebookIcon>
+                          <FacebookIcon  logoFillColor="white" round={true} ></FacebookIcon>
                           </FacebookShareButton>
-                          <TwitterShareButton title="Internet Capsule" via="http://localhost:3000/capsule" hashtags="[#React #NetArt]"><TwitterIcon logoFillColor="white" round={true}></TwitterIcon></TwitterShareButton>
-                          <EmailShareButton subject="Look at what I pulled from this Internet Time Capsule!"><EmailIcon logoFillColor="white" round={true}></EmailIcon></EmailShareButton>
+                          <TwitterShareButton className="social-btn" title="Internet Capsule" via="http://localhost:3000/capsule" hashtags="[#React #NetArt]"><TwitterIcon logoFillColor="white" round={true}></TwitterIcon></TwitterShareButton>
+                          <EmailShareButton className="social-btn" subject="Look at what I pulled from this Internet Time Capsule!"><EmailIcon logoFillColor="white" round={true}></EmailIcon></EmailShareButton>
                           <button className="delete" ><  img className="trash" src="https://img.icons8.com/wired/64/000000/delete-forever.png" alt="trash" onClick={this.props.textDelete}/></button>
                           <button type="button" className="submit-btn" onClick={this.props.startOverClick}>RESET</button>
                           </div>
